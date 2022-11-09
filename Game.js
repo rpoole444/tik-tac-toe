66dbe14396c9a7fb2ea1475c3f800dbab6016ff2
Reid Poole <poole.reid@gmail.com>
2022-11-08 22:28:57 -0600

    add player class and game class WIP

---
+++ Game.js
@@ -0,0 +1,6 @@
+var player1 = new Player ()
+var player2 = new Player ()
+
+class Game {
+    
+}
---
+++ Player.js
@@ -0,0 +1,10 @@
+class Player {
+        constructor(id, token){
+            this.id = id;
+            this.token = token;
+            this.win = 0
+        }
+        increaseWins(){
+            
+        }
+}
