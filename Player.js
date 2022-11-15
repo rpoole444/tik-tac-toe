eaeca6520488b1a94b8d79a40f493cf191489686
Reid Poole <poole.reid@gmail.com>
2022-11-15 14:12:37 -0600

    clear unused CSS, notes, and unused JS code

--- Player.js
+++ Player.js
@@ -1,18 +1,12 @@
 class Player {
-        constructor(playerName, playerToken){
-            this.name = playerName;
-            this.token = playerToken;
-            this.wins = 0;
-            this.isWinner = 0
-            
-            
-        }
+  constructor(playerName, playerToken) {
+    this.name = playerName;
+    this.token = playerToken;
+    this.wins = 0;
+    this.isWinner = 0;
+  }
 
-        increaseWins(){
-          this.wins += 1
-          
-        }
-
-        
+  increaseWins() {
+    this.wins += 1;
+  }
 }
-
